574c67058959660860738463e94bc4fd9cf0ec1e
PaulMBarker <paul.barker@unsw.edu.au>
2019-10-25 17:25:23 +1100

    Add files via upload

--- accelerated_newton.m
+++ accelerated_newton.m
@@ -1,94 +1,93 @@
-function [x,x_error,P] = accelerated_newton(func,dfunc,x_initial,convergence_tolerence,Number_of_iterations)
-
-% accelerated_newton                 Accelerated version of Newton's Method
-%==========================================================================
+function [x,x_err,x_h] = accelerated_newton(func,dfunc,x0,conv_tol,n_iter)
+ 
+% accelerated_newton                Accelerated version of Newton's Method
+%=========================================================================
 %
 % USAGE:
-%  [x,x_error,P] = accelerated_newton(@func,@dfunc,x_initial,deltax,Number_of_iterations)
+%  [x,x_err,x_h] = accelerated_newton(@func,@dfunc,x0,conv_tol,n_iter) 
 %
 % DESCRIPTION:
-%  This Accelerated version of Newton method for approximating the root of 
-%  a univariate function converges faster, with the convergence order being
-%  (sqrt(3) + 1) compared with 2 for Newton method. 
+%  This accelerated version of Newton’s method for approximating the 
+%  root of a univariate function has the order of convergence being 
+%  (sqrt(3) + 1) ~ 2.732 compared with the convergence order of 
+%  Newton’s method which is 2. 
 %
-% Inputs
-%  func                   name of the function
-%  dfunc                  name of the function's derivative
-%  x_initial              Initial estimate of the root
-%  convergence_tolerence  convergence tolerance for the root (x)
-%  Number_of_iterations   maximum number of iterations
+% INPUTS: 
+%  func          name of the function 
+%  dfunc         name of the function's derivative 
+%  x0            initial estimate of the root 
+%  conv_tol      convergence tolerance for the root (x)
+%  n_iter        maximum number of iterations 
 %
 %  func & dfunc need to be inputted with an @ prefix. 
 %
-% OUTPUT:
-%   x        solution: the root
-%   x_error  the change to x on the last iteration 
-%   P        History vector of the values of x during the iterations
+% OUTPUTS: 
+%  x             solution, that is, the root 
+%  x_err         the change to x on the last iteration 
+%  x_h           history vector of the values of x during the iterations 
 %
-% AUTHOR: 
-%  Trevor McDougall, Simon Wotherspoon & Paul Barker   [ help@teos-10.org ]
+% AUTHORS: 
+%  Trevor McDougall, Simon Wotherspoon & Paul Barker  [ help@teos-10.org ] 
 %
-% VERSION NUMBER: 1 (17th September, 2019)
+% VERSION NUMBER: 1 (25th October, 2019) 
 %
 % REFERENCES:
 %  McDougall, T.J., S.J. Wotherspoon and P.M. Barker, 2019: An Accelerated
 %   version of Newton’s Method with convergence order "sqrt(3) + 1".   
-%   Results in Applied Mathematics (submitted). 
+%   Results in Applied Mathematics, in press. 
 %
-%==========================================================================
-
+%=========================================================================
+ 
 if ~isa(func,'function_handle' ) || ~isa(dfunc,'function_handle' )
-    error('the function must be a handle (add an "@" at the begening of the function name)')
+  error('function must be a handle (add "@" before the function name)') 
+end
+ 
+if ~exist('n_iter','var')
+  n_iter = 30; 
+end
+ 
+if ~exist('conv_tol','var')
+  conv_tol = 1e-15; 
 end
 
-if ~exist('Number_of_iterations','var')
-    Number_of_iterations = 30;
-end
-
-if ~exist('convergence_tolerence','var')
-    convergence_tolerence = 1e-16;
-end
-
-    x = x_initial;
-    P = nan(Number_of_iterations,1);
-    P(1) = x;
-    f = func(x);
-    df = dfunc(x);
-    delta = f./df;
-    x_23 = x - 2.*delta./3;
-    df_23 = dfunc(x_23);
-    R = df_23./df;
-    disc = max(0, 0.75*R - 0.5);
-    FF = 0.5 + sqrt(disc);
-    x_old = x;
-    x = x_old - delta./FF;
-    f_old = f;
-    df_old = df;
+x = x0;
+x_h = nan(n_iter+1,1); 
+x_h(1) = x;
+ 
+for k = 1:n_iter
+  f = func(x);
+  df = dfunc(x);
+  delta = f./df; % -delta is the increment of the regular Newton’s Method.
     
-for k = 1:Number_of_iterations
-    f = func(x);
-    df = dfunc(x);
-    if df ~= 0
-        delta = f./df;
-        d2f = (4.*df + 2.*df_old - 6.*(f - f_old)./(x - x_old))./(x - x_old);
-        A2_hat = 0.5.*d2f./df;
-        disc = max(0, 0.25 - A2_hat.*delta);
-        FF = 0.5 + sqrt(disc);
-        x_old = x;
-        x = x_old - delta./FF;
-        df_old = df;
-        f_old = f;
-    else
-        break
-    end
+  if k == 1
+    df_23 = dfunc(x - 2.*delta./3);    % An extra value of the derivative.
+    FF = 0.5 + sqrt(max(0, 0.75.*df_23./df - 0.5));    % FF >= 0.5 always.
+    % This value of FF is for our accelerated version of Jarratt’s method.
+    % If one prefers this first step to be simply a regular Newton step,
+    % then replace the above two lines of code with FF = 1.
+  else
+    d2f = (4.*df + 2.*df_old - 6.*(f - f_old)./(x - x_old))./(x - x_old);
+    A2_hat = 0.5.*d2f./df;
+    FF = 0.5 + sqrt(max(0, 0.25 - A2_hat.*delta));     % FF >= 0.5 always.
+  end
     
-    P(k+1) = x;
-    x_error = abs(x_old - x);
+  x_old = x;
+  x = x_old - delta./FF;
+  x_h(k+1) = x;
+  x_err = abs(x_old - x);
     
-    if ((x_error < convergence_tolerence) && (abs(f) < convergence_tolerence))
-        P(k+2:Number_of_iterations) = [];
-        break
-    end
+  if ((x_err < conv_tol) && (abs(f) < conv_tol))
+    x_h(k+2:n_iter+1) = [];
+    break
+  end
+  
+  df_old = df; f_old = f;
+    
+  if k == n_iter
+    disp('The function did not converge') 
+  end
+    
 end
-
-end
+ 
+end 
+ 
